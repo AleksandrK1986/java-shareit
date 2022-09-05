f63bc524800bedab6405370b76a2239533969d65
AleksandrK1986 <kuchinsky-a-y@yandex.ru>
2022-09-05 22:17:27 +0300

    refactor: исправить замечания к ФЗ 13 спринта

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 import ru.practicum.shareit.booking.model.Status;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.dto.UserDto;
 
 import java.time.LocalDateTime;
